2d0085d8833bcd7aa78581ae96c79eea1284302f
Bert Kleewein <bertk@microsoft.com>
2022-12-30 21:29:35 -0800

    fix new_machine script

--- new-machine.sh
+++ new-machine.sh
@@ -14,20 +14,20 @@ sudo apt install -y \
 mkdir -p ~/bin 
 curl https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/bin/z.sh && chmod +x ~/bin/z.sh
 
-mkdir -p ~/vim/backup  
+mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/undos
 
-rm ~/.vimrc 
-rm ~/.gitconfig
-rm ~/.tmux.conf
+rm ~/.vimrc || true
+rm ~/.gitconfig || true
+rm ~/.tmux.conf || true
 
 sudo -E sh -c "echo \"${USER}   ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers"
 echo "User ${USER} added to sudoers.  Use 'sudo visudo' to verify."
 
 
 cp ${script_dir}/vimrc ~/.vimrc
-cp ${Script_dir}/tmux.conf / ~/.tmux.conf
+cp ${script_dir}/tmux.conf ~/.tmux.conf
 
 echo "success"
 
