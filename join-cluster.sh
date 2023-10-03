4f56da1fb20360257b5f97f483c6d5cb959a3c43
Ubuntu <azureuser@vm-01-a.2ttyxfhrce2ubpwruywgztrqkf.xx.internal.cloudapp.net>
2023-10-03 21:36:31 +0000

    fix script typo

--- join-cluster.sh
+++ join-cluster.sh
@@ -4,5 +4,5 @@ trap 'echo ERROR on line ${LINENO}' ERR
 CLUSTER_IP=$1
 NODE_TOKEN="$2"
 
-curl -sfL https://get.k3s.io | K3S_URL=https://${CLUSTER_UP}:6443 K3S_TOKEN="${NODE_TOKEN}" sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://${CLUSTER_IP}:6443 K3S_TOKEN="${NODE_TOKEN}" sh -
 
