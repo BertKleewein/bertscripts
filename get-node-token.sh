b56e48a190a2eff915c0c9aedde58d04b8351e22
Bert Kleewein <bertk@microsoft.com>
2023-10-03 21:34:07 +0000

    scrpts to join k3s clusters

---
+++ get-node-token.sh
@@ -0,0 +1,4 @@
+set -exuo pipefail
+trap 'echo ERROR on line ${LINENO}' ERR
+
+sudo cat /var/lib/rancher/k3s/server/node-token
---
+++ join-cluster.sh
@@ -0,0 +1,8 @@
+set -exuo pipefail
+trap 'echo ERROR on line ${LINENO}' ERR
+
+CLUSTER_IP=$1
+NODE_TOKEN="$2"
+
+curl -sfL https://get.k3s.io | K3S_URL=https://${CLUSTER_UP}:6443 K3S_TOKEN="${NODE_TOKEN}" sh -
+
